62029b5d1b0f4a84ed363473c0b0d18e8a1c0e67
cloudsim-dev\ym232f <hassandaru@gmail.com>
2023-11-28 21:53:02 +1000

    update main body css

--- src/components/main.js
+++ src/components/main.js
@@ -4,14 +4,14 @@ export default function Main() {
 
     return (
         <div className="main">
-       <h1 className="heading">Fun facts about React</h1>
-       <ul>
-            <li>was first released in 2013</li>
-            <li>was originally created by jordan walke</li>
-            <li>has well over 100K stars on Github</li>
-            <li>is maintained by facebook</li>
-            <li>powers thousands of enterprise apps, including mobile apps</li>
-       </ul>
+            <h1 className="heading">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>was first released in 2013</li>
+                <li>was originally created by jordan walke</li>
+                <li>has well over 100K stars on Github</li>
+                <li>is maintained by facebook</li>
+                <li>powers thousands of enterprise apps, including mobile apps</li>
+             </ul>
        </div>
     );
 }
